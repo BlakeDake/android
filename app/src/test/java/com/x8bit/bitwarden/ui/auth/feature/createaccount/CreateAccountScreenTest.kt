6cc3cbd05764f23a8dc6f17d18e3a2dbdd76a6a6
Alessandro Fedriga <s320136@studenti.polito.it>
2025-12-14 11:04:15 +0100

    accept policies should be toggled on or off according to the state

--- app/src/test/java/com/x8bit/bitwarden/ui/auth/feature/createaccount/CreateAccountScreenTest.kt
+++ app/src/test/java/com/x8bit/bitwarden/ui/auth/feature/createaccount/CreateAccountScreenTest.kt
@@ -94,15 +94,17 @@ class CreateAccountScreenTest : BaseComposeTest() {
     @Test
     fun `accept policies should be toggled on or off according to the state`() {
         composeTestRule
-            .onNodeWithText("By activating this switch you agree", substring = true)
+            .onNodeWithContentDescription("AcceptPoliciesToggle")
+            .performScrollTo()
             .assertIsOff()
 
         mutableStateFlow.update { it.copy(isAcceptPoliciesToggled = true) }
 
         composeTestRule
-            .onNodeWithText("By activating this switch you agree", substring = true)
+            .onNodeWithContentDescription("AcceptPoliciesToggle")
             .assertIsOn()
     }
+
 
     @Test
     fun `accept policies click should send AcceptPoliciesToggle action`() {
@@ -112,15 +114,6 @@ class CreateAccountScreenTest : BaseComposeTest() {
             .performClick()
         verify { viewModel.trySendAction(AcceptPoliciesToggle(true)) }
     }
-
-
-
-
-
-
-
-
-
 
 
     @Test
