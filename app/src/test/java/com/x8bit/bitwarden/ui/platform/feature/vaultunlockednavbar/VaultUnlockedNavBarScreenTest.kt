75288ef904d475fe0331fded479fe6016bdfe916
Alessandro Fedriga <s320136@studenti.polito.it>
2025-12-20 17:37:48 +0100

    fix VaultUnlockedNavBarScreenTest

--- app/src/test/java/com/x8bit/bitwarden/ui/platform/feature/vaultunlockednavbar/VaultUnlockedNavBarScreenTest.kt
+++ app/src/test/java/com/x8bit/bitwarden/ui/platform/feature/vaultunlockednavbar/VaultUnlockedNavBarScreenTest.kt
@@ -1,4 +1,3 @@
-/*
 package com.x8bit.bitwarden.ui.platform.feature.vaultunlockednavbar
 
 import androidx.compose.ui.test.onNodeWithText
@@ -42,7 +41,7 @@ class VaultUnlockedNavBarScreenTest : BaseComposeTest() {
                 VaultUnlockedNavBarScreen(
                     viewModel = viewModel,
                     navController = fakeNavHostController,
-                    onNavigateToVaultAddItem = { _, _, _ -> },
+                    onNavigateToVaultAddItem = { },
                     onNavigateToVaultItem = {},
                     onNavigateToVaultEditItem = {},
                     onNavigateToAddSend = {},
@@ -57,6 +56,7 @@ class VaultUnlockedNavBarScreenTest : BaseComposeTest() {
                     onNavigateToSetupAutoFillScreen = {},
                     onNavigateToSetupUnlockScreen = {},
                     onNavigateToImportLogins = {},
+                    onNavigateToAddFolderScreen = {},
                 )
             }
         }
@@ -246,4 +246,3 @@ private val DEFAULT_STATE = VaultUnlockedNavBarState(
         settingsTabNotificationCount = 0,
     ),
 )
-*/
